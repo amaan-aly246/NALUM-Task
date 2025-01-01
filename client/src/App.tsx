15b690006c927f23b4dfa3e282d57838d4e1d895
amaan <amaan.ali.ug22@nsut.ac.in>
2025-01-01 09:02:07 +0530

    react , tailwind , shadcn added

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+import { Button } from './components/ui/button'
+function App() {
+
+  return (
+    <>
+     <h2 className='bg-red-300  '>Hello world</h2>
+     <Button>Click Button</Button>
+    </>
+  )
+}
+
+export default App
